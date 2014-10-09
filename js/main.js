39546a3535f7faf9efbdf137764e729598e77851
Jared Lebel <jared.lebel87@gmail.com>
2014-10-09 09:10:06 -0400

    Added waypoints plugin.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var $win = $(window);
 var $sun = $('.sun');
-var $sun-section = $('.sun-section');
+var $sunSection = $('.sun-section');
 
 $win.on('scroll', function() {
     var scrollPos = $win.scrollTop();
         
     $sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
     
-    4sun-section.css('background-position', 'center' + scrollPos / 2 + 'px')
+    $sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
